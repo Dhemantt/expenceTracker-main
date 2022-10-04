982c16e33ba338c39fcacd949e25bac65295a4b0
Dhemantt <dixithemant02>
2022-10-04 20:57:39 +0530

    expense tracker project with axios

--- script.js
+++ script.js
@@ -2,68 +2,114 @@ var addExpenceList = document.getElementById('second-row') ;
 const itemList = document.getElementById('expenceAdd');
 
 
+// itemList.addEventListener("click" , addItem);
 
+// addExpenceList.addEventListener('click', deleteItem);
+// addExpenceList.addEventListener("click", editItem);
 
-itemList.addEventListener("click" , addItem);
-
-addExpenceList.addEventListener('click', deleteItem);
-addExpenceList.addEventListener("click", editItem);
-
-function addItem (e) {
+function onsignup(e) {
     e.preventDefault();
     //get the value of each id
-    const newItemAmount = document.getElementById("amount-id").value;
-    const newItemDescription =document.getElementById("description-id").value;
-    const newItemCatagory = document.getElementById("catagory-id").value;
+    // const newItemAmount = document.getElementById("amount-id").value;
+    // const newItemDescription = document.getElementById("description-id").value;
+    // const newItemCatagory = document.getElementById("catagory-id").value;
+    const newItemAmount = e.target.amountid.value
+            // localStorage.setItem('Name', name )
+            var newItemDescription = e.target.descriptionid.value
+            // localStorage.setItem('Email',email)
+            var newItemCatagory = e.target.catagoryid.value
+    const obj = {
+        newItemAmount,
+        newItemDescription,
+        newItemCatagory
+    }
 
-    //creation of li
-    var newLi = document.createElement('li');
-    
-    //now add value to the li 
-    newLi.textContent = `${newItemAmount} ${newItemDescription}  ${newItemCatagory}`;
-
-    //add button edit and delete
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.className = "delete";
-
-    deleteBtn.appendChild(document.createTextNode("delete"));
-
-    newLi.appendChild(deleteBtn);
-
-
-    const editBtn = document.createElement("button");
-    editBtn.className = 'edit';
-    editBtn.id = 'edit-id';
-    editBtn.appendChild(document.createTextNode("edit"));
-
-    newLi.appendChild(editBtn);
-
-    addExpenceList.appendChild(newLi);
-
-   
-
-
-
+    //send api post request
+    axios.post('https://crudcrud.com/api/2ddae4226ba84265909b36e19e96148d/ExpenseData', obj)
+        .then((response) => {
+            AdduserToDom(response.data)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
 
-function deleteItem(e) {
-    if(e.target.classList.contains("delete")) {
-        if(confirm("Are u wanna delete ?")){
-            const li = e.target.parentElement;
-            addExpenceList.removeChild(li);
+window.addEventListener("DOMContentLoaded", () => {
+    console.log('hiii')
+    axios.get('https://crudcrud.com/api/2ddae4226ba84265909b36e19e96148d/ExpenseData')
+        .then((response) => {
+        console.log(response)
+        for(let i=0; i<response.data.length; i++){
+            AdduserToDom(response.data[i]);
         }
+        if(localStorage.getItem(user.email)!== null){
+        removeUserFromScreen(user.email);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+function AdduserToDom(user) {
+    const childnode = `<li id=${user._id}> ${user.newItemAmount} - ${user.newItemDescription} - ${user.newItemCatagory} 
+        <button onclick="editItem('${user._id}',${user.newItemAmount},'${user.newItemDescription}','${user.newItemCatagory}')">Edit</button>
+        <button onclick="deleteItem('${user._id}')">Delete</button></li>`
+    addExpenceList.innerHTML = addExpenceList.innerHTML + childnode
+}
+
+function deleteItem(id) {
+    console.log(id);
+    axios.delete(`https://crudcrud.com/api/2ddae4226ba84265909b36e19e96148d/ExpenseData/${id}`)
+        .then((response) => {
+       removeUserFromScreen(id)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    // if(e.target.classList.contains("delete")) {
+    //     if(confirm("Are u wanna delete ?")){
+    //         const li = e.target.parentElement;
+    //         addExpenceList.removeChild(li);
+    //     }
+    // }
+}
+function removeUserFromScreen(id){
+    // const parentnode = document.getElementById('listOfUsers')
+    const childnodetobedeleted = document.getElementById(id)
+    if(childnodetobedeleted){
+        addExpenceList.removeChild(childnodetobedeleted);
     }
 }
 
-function editItem(e) {
+
+function editItem(id,amount,description,category) {
    // e.preventDefault();
-    if(e.target.classList.contains("edit")) {
-        var details =document.getElementsByTagName(li).value;
-        details.textContent.contentEditable= true;
+    const newItemAmount = document.getElementById('amountid')
+    newItemAmount.value = amount;
 
-     
+    const newItemDescription = document.getElementById('descriptionid')
+    newItemDescription.value = description;
 
-    } 
+    const newItemCatagory = document.getElementById('catagoryid')
+    newItemCatagory.value = category;
+
+    const userdata = {
+        amount,
+        description,
+        category
+    }
+
+    const childnodetobedeleted = document.getElementById(id)
+    console.log(childnodetobedeleted)
+    // addExpenceList.removeChild(childnodetobedeleted);
+    axios.put(`https://crudcrud.com/api/2ddae4226ba84265909b36e19e96148d/ExpenseData/${id}`,userdata)
+        .then((response) => {
+            removeUserFromScreen(id)
+            AdduserToDom(userdata)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 }
 
